0847e5f30a3d7d82b2aa7d1cced7de8f49a366e5
Pawan Kalyan <pawankalyan1614@gmail.com>
2023-05-26 22:02:47 +0530

    removed useless code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sici-aura-intern-evaluation
+
+## Careers Page
+
+- This is a evaluation exam for the Sici-Aura SDE intern Position.
+- We were given a figma design 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-const Jobs = require('./models/jobs');
 const jobs_router = require('./routes/jobs_route');
 
 require('dotenv').config();
